bb9c5fbe16d05bdddca9f6b7af4eb5ce3ed3fd7a
paraflu <andrea.forlin@gmail.com>
2022-04-12 10:37:58 -0500

    initial

---
+++ bw/README.md
@@ -0,0 +1,11 @@
+# B/W
+
+
+Ip `10.10.10.3`
+
+open port 80
+
+# Available directory
+
+admin
+assets
---
+++ bw/index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+<script>
+top.window.debug = true;
+top.debug = true;
+		</script>
+</head>
+	<body>
+<script	>
+document.getElementsByTagName('body')[0].innerText = new Date();
+</script>
+	</body>
+</html>
